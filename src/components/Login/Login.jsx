94b0662e4a27d1ba063bc636f0463737e6a9ceb2
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-07 01:48:00 +0600

    login page is now successfully responsive

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,51 +3,93 @@ import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-    return (
-      <div>
-        <div className="min-h-screen bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")' }}>
-          <div className="flex justify-end">
-            <div className="bg-white min-h-screen w-1/2 flex justify-center items-center">
-              <div>
-                <form>
-                  <div>
-                    <span className="text-sm text-gray-900">Welcome back</span>
-                    <h1 className="text-2xl font-bold">Login to your account</h1>
-                  </div>
-{/* email */}
-                  <div className="my-3">
-                    <label className="block text-md mb-2" htmlFor="email">Email</label>
-                    <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="email" placeholder="Email" />
-                  </div>
-{/* password */}
-                  <div className="mt-5">
-                    <label className="block text-md mb-2" htmlFor="password">Password</label>
-                    <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" name="password" placeholder="Password" />
-                  </div>
+  return (
+    <div>
+      {/* Background image div */}
+      <div className="hidden md:block min-h-screen bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")' }}>
+        {/* Content is placed over the background image */}
+        <div className="md:flex md:justify-end">
+          <div className="bg-white min-h-screen w-1/2 flex justify-center items-center">
+            <div>
+            <form className="px-6 py-8">
+                <div>
+                  <span className="text-sm text-gray-900">Welcome back</span>
+                  <h1 className="text-2xl font-bold">Login to your account</h1>
+                </div>
+                {/* email */}
+                <div className="my-3">
+                  <label className="block text-md mb-2" htmlFor="email">Email</label>
+                  <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="email" placeholder="Email" />
+                </div>
+                {/* password */}
+                <div className="mt-5">
+                  <label className="block text-md mb-2" htmlFor="password">Password</label>
+                  <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" name="password" placeholder="Password" />
+                </div>
 
-                  <div className="flex justify-between mt-2">
-                    <div>
-                      <input className="cursor-pointer mr-2" type="radio" name="rememberme" />
-                      <span className="text-sm">Remember Me</span>
-                    </div>
-                    <span className="text-sm text-blue-700 hover:underline cursor-pointer">Forgot password?</span>
-                  </div>
+                <div className="flex justify-between mt-2">
                   <div>
-                    <button className="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100">Login now</button>
-                    <div className="flex space-x-2 justify-center items-center bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
-                      <FcGoogle></FcGoogle>
-                      <button>Or sign-in with Google</button>
-                    </div>
+                    <input className="cursor-pointer mr-2" type="radio" name="rememberme" />
+                    <span className="text-sm">Remember Me</span>
                   </div>
-                </form>
-                <p className="mt-8">Don't have an account? <Link to='/register' className="cursor-pointer text-sm text-blue-600 hover:underline hover:text-red-600">Register Here</Link></p>
-              </div>
+                  <span className="text-sm text-blue-700 hover:underline cursor-pointer">Forgot password?</span>
+                </div>
+                <div>
+                  <button className="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100">Login now</button>
+                  <div className="flex space-x-2 justify-center items-center bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
+                    <FcGoogle />
+                    <button>Or sign-in with Google</button>
+                  </div>
+                </div>
+              </form> 
+              <p className="mt-8">Don't have an account? <Link to='/register' className="cursor-pointer text-sm text-blue-600 hover:underline hover:text-red-600">Register Here</Link></p>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Login;
-  
+      
+      {/* Centered form on small screens */}
+      <div className="md:hidden min-h-screen flex justify-center items-center">
+        <div className="bg-white w-4/5 sm:w-3/5 md:w-2/5 lg:w-2/5 p-4">
+         
+            {/* Form contents for small devices */}
+           
+            <form className="px-6 py-8">
+                <div>
+                  <span className="text-sm text-gray-900">Welcome back</span>
+                  <h1 className="text-2xl font-bold">Login to your account</h1>
+                </div>
+                {/* email */}
+                <div className="my-3">
+                  <label className="block text-md mb-2" htmlFor="email">Email</label>
+                  <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="email" placeholder="Email" />
+                </div>
+                {/* password */}
+                <div className="mt-5">
+                  <label className="block text-md mb-2" htmlFor="password">Password</label>
+                  <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" name="password" placeholder="Password" />
+                </div>
+
+                <div className="flex justify-between mt-2">
+                  <div>
+                    <input className="cursor-pointer mr-2" type="radio" name="rememberme" />
+                    <span className="text-sm">Remember Me</span>
+                  </div>
+                  <span className="text-sm text-blue-700 hover:underline cursor-pointer">Forgot password?</span>
+                </div>
+                <div>
+                  <button className="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100">Login now</button>
+                  <div className="flex space-x-2 justify-center items-center bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
+                    <FcGoogle />
+                    <button>Or sign-in with Google</button>
+                  </div>
+                </div>
+            
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

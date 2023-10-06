119bf4c644a12be8af0570a5cefbe9d9f0d2434f
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-06 22:53:31 +0600

    firebase install and config setup done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR0UyXMRlaxMqEcTdyrROulmFR4dbwqEo",
+  authDomain: "gaming-tech-events.firebaseapp.com",
+  projectId: "gaming-tech-events",
+  storageBucket: "gaming-tech-events.appspot.com",
+  messagingSenderId: "652727838012",
+  appId: "1:652727838012:web:34328602e84925a03c2a39"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;

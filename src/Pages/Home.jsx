6c7e4ed6b92a4e7cf34ee94908ad94816c492818
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-08 16:31:31 +0600

    searchbar added in banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,6 +34,16 @@ const Home = () => {
                 connect with fellow enthusiasts. Dive into the future of
                 entertainment and innovation with us.
               </p>
+              <div className="mt-3">
+              <div className="form-control">
+  <div className="input-group">
+    <input type="text" placeholder="Search…" className="input input-bordered" />
+    <button className="btn btn-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+</div>
+              </div>
             </div>
           </div>
         </div>

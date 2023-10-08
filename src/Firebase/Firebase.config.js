9f70e045db69924854f866a17bcf1981f8a434e1
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-09 02:33:19 +0600

    enviornment variable created

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR0UyXMRlaxMqEcTdyrROulmFR4dbwqEo",
-  authDomain: "gaming-tech-events.firebaseapp.com",
-  projectId: "gaming-tech-events",
-  storageBucket: "gaming-tech-events.appspot.com",
-  messagingSenderId: "652727838012",
-  appId: "1:652727838012:web:34328602e84925a03c2a39"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 
